2ffc7a26c4460bb2ad534450cb47048ddb7b2592
Julien Calixte <julienc@bam.tech>
2023-05-21 20:33:30 +0200

    add class to image layouts

--- layouts/image-left.vue
+++ layouts/image-left.vue
@@ -1,14 +1,14 @@
 <script setup lang="ts">
 import { useBackground } from "../hooks/useBackground"
 
-const props = defineProps<{ image: string }>()
+const props = defineProps<{ image: string; class?: string }>()
 const { imageStyle } = useBackground(props.image)
 </script>
 
 <template>
   <div class="grid grid-cols-2 w-full h-full">
     <div :style="imageStyle"></div>
-    <div class="slidev-layout">
+    <div class="slidev-layout" :class="props.class">
       <slot />
     </div>
   </div>
